1c121b98cb19b3cb31d9479a264abf91a110a614
Armando lopez Torres <mandoveloper@gmail.com>
2023-05-07 03:04:19 -0500

    Coomit inicial

---
+++ src/main/java/com/cibertec/edu/DAWII_CL1_ARMANDO_LOPEZ_TORRES/DawiiCl1ArmandoLopezTorresApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.DAWII_CL1_ARMANDO_LOPEZ_TORRES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1ArmandoLopezTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1ArmandoLopezTorresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/edu/DAWII_CL1_ARMANDO_LOPEZ_TORRES/DawiiCl1ArmandoLopezTorresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.DAWII_CL1_ARMANDO_LOPEZ_TORRES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1ArmandoLopezTorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
